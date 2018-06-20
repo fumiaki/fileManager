87b223b9eb501a176742f458e5860b16875c9a0e
fumiaki <fumiaki.oshima@gmail.com>
2018-06-20 19:41:09 +0900

    feat: add "convert to csv" function

--- fileManager.mjs
+++ fileManager.mjs
@@ -1,33 +1,39 @@
 import fs from "fs"
 import path from "path"
 import csv from "csv"
-import stringify from 'csv-stringify-as-promised'
+//import stringify from 'csv-stringify-as-promised'
+import stringify from 'csv-stringify/lib/sync'
 
 const ROOT_PATH = path.resolve("C:/dev")
 
-
-export function readdir(dirPath) {
+export function readdir(dirPath, recursive=false) {
   //console.log(dirPath)
   let list = fs.readdirSync(path.join(ROOT_PATH, dirPath))
   let result = [];
   list.forEach(file => {
-    let filePath = path.join(ROOT_PATH, dirPath, file)
-    let stat = fs.statSync(filePath)
+    let absolutePath = path.join(ROOT_PATH, dirPath, file)
+    let stat = fs.statSync(absolutePath)
     stat.name = file
-    stat.path = dirPath
-    stat.absolutePath = filePath
+    stat.path = path.posix.join(dirPath)
+    stat.absolutePath = absolutePath
     stat.isDirectory = stat.isDirectory()
     result.push(stat)
+
+    if(recursive && stat.isDirectory) {
+      result.push(...readdir(path.join(dirPath, file), true))
+    }
     //console.log(file, stat.size, stat.path, stat.isDirectory())
   })
 
   return result
 }
 
+const DATEFORMATTER = {
+  date: value => value.toLocaleString()
+}
 export async function readdirCsv(dirPath) {
-  let json = readdir(dirPath)
-  let csvStr = await stringify(json)
+  let json = readdir(dirPath, true)
+  let csvStr = await stringify(json, {header:true, formatters:DATEFORMATTER})
 
-  console.log(csvStr)
   return csvStr
 }
